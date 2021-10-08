c6a5ef3b4fe4964d4b67d75b1922589a5148f20c
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-10-08 08:21:18 +0700

    CircleImage

---
+++ CircleImage/app/src/main/java/deso1/dinhtrongdat/crcleimageview/MainActivity.java
@@ -0,0 +1,14 @@
+package deso1.dinhtrongdat.crcleimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
